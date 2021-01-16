88ac7ac77d92c816ae659f0a665659fe40bc903b
noname <no@mail.com>
2021-01-16 12:34:01 +0100

    gestionnaire d'erreur :ok_hand:

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,16 +1,23 @@
 import express from 'express';
-import json from 'express';
+import 'express-async-errors'
+const bodyParser = require('body-parser')
 import {currentUserRouter} from './routes/users/current'
 import {loginRouter} from './routes/users/login'
 import {logoutRouter} from './routes/users/logout'
 import {signupRouter} from './routes/users/signup'
+import {errorHandler} from './middlewares/error-handler'
+import {NotFoundError} from './errors/not-found-error'
+
 
 const port = 3000
 const app = express();
-app.use(json())
-app.use(currentUserRouter)
+app.use(bodyParser.json())
 app.use(signupRouter)
+app.use(currentUserRouter)
 app.use(logoutRouter)
 app.use(loginRouter)
-
+app.all('*', (req, res) => {
+    throw new NotFoundError();
+})
+app.use(errorHandler)
 app.listen(port, () => console.log("listening on port ", port));
--- auth/src/routes/users/signup.ts
+++ auth/src/routes/users/signup.ts
@@ -1,7 +1,24 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
+import {body, validationResult} from 'express-validator';
+import {RequestValidationError} from "../../errors/request-validation-error";
+import {DatabaseConnectionError} from "../../errors/database-connection-error";
 
 const router = express.Router();
-router.post('/api/v1/users/signup', (req, res) => {
-    res.send("signup")
-})
+router.post('/api/v1/users/signup', [
+        body('email')
+            .isEmail()
+            .withMessage("Please enter a valid email address"),
+        body('password')
+            .trim()
+            .isLength({min: 8, max: 1000})
+            .withMessage("Password is too short")
+    ], (req: Request, res: Response) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            throw new RequestValidationError(errors.array())
+        }
+        const {email, password} = req.body;
+        throw new DatabaseConnectionError()
+    }
+)
 export {router as signupRouter};
