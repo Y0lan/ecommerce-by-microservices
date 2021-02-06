24276718b64fa31063e2c432139371b4d2d23f80
Yolan Maldonado <register@yolan.dev>
2021-02-06 18:36:33 +0100

    test

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,7 @@ import { app } from "./app";
 
 const port = 3000
 const start = async () => {
+    console.log("Starting authentification Service...")
 
     if(!process.env.JWT_SECRET_KEY){
         throw new NotFoundError("JWT_SECRET_KEY MISSING IN ENV")
