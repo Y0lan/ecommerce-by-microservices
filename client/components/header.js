ada41a96b7f6f716b689d2d85601517851d12437
Yolan Maldonado <register@yolan.dev>
2021-02-10 23:06:12 +0100

    TEST CLOUD

--- client/components/header.js
+++ client/components/header.js
@@ -19,7 +19,7 @@ const fn = ({currentUser}) => {
         })
     return <nav className="navbar navbar-light bg-light">
         <Link href='/'>
-            <a className="navbar-brand">micro.io 🥰</a>
+            <a className="navbar-brand">micro.ioii 🥰</a>
         </Link>
         <div className="div-flex justify-content-end">
             <ul className="nav d-flex align-items-center">
