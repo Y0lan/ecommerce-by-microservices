b5eec3c627dc95b9daaedd2a63a45b6d9ea64efa
Yolan Maldonado <register@yolan.dev>
2021-07-26 14:30:44 +0200

    PUBLISH MEEEE

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -4,7 +4,7 @@ import {Order} from "../models/order";
 import {natsWrapper} from "../nats-wrapper";
 import {OrderCancelledPublisher} from "../events/publishers/order-cancelled-publisher";
 
-console.log("push")
+console.log("pus")
 const router = express.Router()
 router.delete('/api/v1/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
     const {orderId} = req.params;
